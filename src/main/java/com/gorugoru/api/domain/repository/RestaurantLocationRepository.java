ebab2981c1b9c44b1807d393d743b9672f503f67
ChickenPaella <ChickenPaella@github.com>
2017-02-23 00:40:23 +0900

    동 리스트 출력

---
+++ src/main/java/com/gorugoru/api/domain/repository/RestaurantLocationRepository.java
@@ -0,0 +1,19 @@
+package com.gorugoru.api.domain.repository;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.gorugoru.api.domain.model.RestaurantLocation;
+
+@Transactional
+public interface RestaurantLocationRepository extends JpaRepository<RestaurantLocation, Long> {
+	
+	@Query("select dong from RestaurantLocation t where t.sigugun = :sigugun")
+	public abstract List<String> findBySigugun(@Param("sigugun") String sigugun);
+	
+}

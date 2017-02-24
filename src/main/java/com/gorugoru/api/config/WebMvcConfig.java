1abe27091ccff3a1e0602b2e12eff8e881ab60fd
ta3ke <admin@ta3ke.com>
2017-02-24 10:33:20 +0900

    spring web mvc 설정 통합 WebMvcConfigurerAdapter 사용설정 제거 WebMvcConfigurationSupport만 사용
    Signed-off-by: ta3ke <admin@ta3ke.com>

--- src/main/java/com/gorugoru/api/config/WebMvcConfig.java
+++ src/main/java/com/gorugoru/api/config/WebMvcConfig.java
@@ -1,24 +1,35 @@
 package com.gorugoru.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 /**
  * MVC설정
+ * WebMvcConfigurerAdapter 대신 WebMvcConfigurationSupport 사용
  * http://www.thymeleaf.org 사용
  * springboot 자동설정 참고
  * https://github.com/spring-projects/spring-boot/blob/master/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/web/WebMvcAutoConfiguration.java
  * @author Administrator
  */
 @Configuration
-public class MvcConfig extends WebMvcConfigurerAdapter {
-
-    @Override
+public class WebMvcConfig extends WebMvcConfigurationSupport{
+	
+	@Bean
+	public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+		RequestMappingHandlerMapping handlerMapping = super.requestMappingHandlerMapping();
+		handlerMapping.setUseSuffixPatternMatch(false);//ignore url file extension(comma .)
+		return handlerMapping;
+	}
+	
+	@Override
     public void addViewControllers(ViewControllerRegistry registry) {
     	//assign templates non-controller used. 단순 뷰페이지 호출
     	registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/admin/login").setViewName("admin/login");
     }
+	
 }

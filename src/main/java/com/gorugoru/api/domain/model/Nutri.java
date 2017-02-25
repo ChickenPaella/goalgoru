4c7cdd79a14d8460240cf558849311de39c484b6
ta3ke <admin@ta3ke.com>
2017-02-26 04:01:24 +0900

    영양소 enum json출력 한글 처리
    Signed-off-by: ta3ke <admin@ta3ke.com>

--- src/main/java/com/gorugoru/api/domain/model/Nutri.java
+++ src/main/java/com/gorugoru/api/domain/model/Nutri.java
@@ -1,7 +1,20 @@
 package com.gorugoru.api.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Nutri {
-	Carbohydrate,
-	Protein,
-	Fat
+	Carbohydrate("탄수화물"),
+	Protein("단백질"),
+	Fat("지방");
+	
+	private String value;
+
+	private Nutri(String value) {
+		this.value = value;
+	}
+	
+	@JsonValue
+	public String value(){
+		return value;
+    }
 }

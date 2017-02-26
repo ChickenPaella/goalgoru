8bbc37d04ea9d69092456216714aa775574b4b76
ta3ke <admin@ta3ke.com>
2017-02-26 09:21:36 +0900

    가라 로그인
    Signed-off-by: ta3ke <admin@ta3ke.com>

--- src/main/java/com/gorugoru/api/controller/AuthController.java
+++ src/main/java/com/gorugoru/api/controller/AuthController.java
@@ -11,12 +11,7 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -177,6 +172,9 @@ public class AuthController {
 					UserAuthentication authentication = new UserAuthentication(new SecUser(principal, "null", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER")));
 					tokenAuthenticationService.addAuthentication(response, authentication);
 					
+					//가라 로그인
+					session.setAttribute("principal", principal);
+					
 					//토큰재사용 쿠키
 					Token token = (Token) session.getAttribute(AuthAttr.KAKAO_TOKEN);
 					response.addCookie(new Cookie("ltoat", token.getAccessToken()));
@@ -196,6 +194,10 @@ public class AuthController {
 			logger.info("referer: "+referer);
 			
 			String jwt = response.getHeader(TokenAuthenticationService.AUTH_HEADER_NAME);
+			
+			//가라 로그인
+			String principal = (String) session.getAttribute("principal");
+			jwt = URLEncoder.encode(principal, "UTF-8");
 			
 			URI uri = new URI(referer);
 			String appHost = uri.getScheme()+"://"+uri.getHost()+":"+uri.getPort();
